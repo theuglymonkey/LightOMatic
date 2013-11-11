045ebe323d3fe27ddaffe6edb1bf1676273d4fd2
Robert Filipkowski <rfilip01@gmail.com>
2013-11-10 22:58:19 -0500

    Removed headers, added serial com code
    removed some headers and added some serial com from boost asio

--- VisualStudioCode/LightOMatic/LightOMatic/MicroComSerial.cpp
+++ VisualStudioCode/LightOMatic/LightOMatic/MicroComSerial.cpp
@@ -3,9 +3,14 @@
 
 MicroComSerial::MicroComSerial(void)
 {
+	serialPort = new boost::asio::serial_port(io);
+	io.run();
 }
 
 
 MicroComSerial::~MicroComSerial(void)
 {
+	 serialPort->close();
+	 delete serialPort;
 }
+
--- VisualStudioCode/LightOMatic/LightOMatic/MicroComSerial.h
+++ VisualStudioCode/LightOMatic/LightOMatic/MicroComSerial.h
@@ -1,14 +1,26 @@
-
-#include <windows.h>
-#include "Serial.h"
+#include <boost/asio.hpp>
+#include <string>
 #pragma once
 
 class MicroComSerial
 {
 private:
-
+	boost::asio::io_service io;
+	boost::asio::serial_port* serialPort;
+	int m_buadRate;
+	std::string m_comPort;
 public:
 	MicroComSerial(void);
 	~MicroComSerial(void);
+	void OpenCom(
+		   const std::string& portname, 
+           int baudrate, 
+           int charactersize, 
+           boost::asio::serial_port_base::parity::type parity, 
+           boost::asio::serial_port_base::stop_bits::type stopbits,
+           boost::asio::serial_port_base::flow_control::type flowcontrol);
+	void CloseCom();
+
+	size_t send(void* data, size_t length);
 };
 
